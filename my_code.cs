3ff8179a6c5936b8442bdc894f55fdd9fb681c68
Mujahid792 <muhammadmujahidali2@gmail.com>
2025-02-13 10:00:52 +0500

    add my code to display name, roll number and department.
    my code to display name, roll number and department.

--- my_code.cs
+++ my_code.cs
@@ -9,3 +9,14 @@ class Program
 
     }
 }
+//my code to display name, roll number and department.
+using system;
+class Program
+{
+     static void Main()
+    {
+    Console.Writeline("My Name is Mujahid");
+    Console.Writeline("Roll number:24-ST-058");
+    Console.Writeline("Departement:Software engineering technology");
+    }
+}
